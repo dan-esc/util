0cdcb256b0a58e425f46ee6812dc109ee1b4601d
Dan Aubin <dan.aubin1@gmail.com>
2022-01-20 15:05:12 +1000

    Add extra letters, random symbols

--- scripts/linux/nicepass4linux.sh
+++ scripts/linux/nicepass4linux.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-#nicepass pw generator for linux systems
-PASSWORD=$(curl -s https://www.dinopass.com/password/strong)
-echo $PASSWORD
+##nicepass - A simple script to produce a secure yet human memorable password
+
+#take output from dinopass.com (decent but not strong enough) 
+DINOPASS=$(curl -s https://www.dinopass.com/password/strong)
 
 #add additional letters and symbols for complexity
+RAND=$(openssl rand -base64 3)
+
+#symbols
+arr[0]="!"
+arr[1]="@"
+arr[2]="#"
+arr[3]="$"
+arr[4]="&"
+arr[5]="="
+rand=$[$RANDOM % ${#arr[@]}]
+RANDSYMBOL=${arr[$rand]}
+
+echo $DINOPASS$RAND$RANDSYMBOL
