cca8521ad54627368b6d2ef8e3d8238b8b96f634
Dan Aubin <dan.aubin1@gmail.com>
2022-01-20 14:16:44 +1000

    remove test

--- scripts/linux/nicepass4linux.sh
+++ scripts/linux/nicepass4linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #nicepass pw generator for linux systems
+PASSWORD=$(curl -s https://www.dinopass.com/password/strong)
+echo $PASSWORD
 
-#Install prerequisties
-#If statement, if 'w3m is installed' then skip if not install
-
-password=$(w3m -dump https://www.dinopass.com/password/strong); echo $password
+#add additional letters and symbols for complexity
